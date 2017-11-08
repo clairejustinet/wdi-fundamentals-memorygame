633ec70b10c5c1fdce9acff20e8a9bef5721da71
clairejustinet <claire.backs@gmail.com>
2017-11-07 20:58:52 -0800

    first pass at memory game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardTwo);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+console.log("user flipped " + cardOne);
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("you found a match!");
+	} else {
+		alert("sorry, try again!");
+	}
+}
