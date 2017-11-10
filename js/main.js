7822366f09373079b1344cd8a8d2c440c50ea57f
clairejustinet <claire.backs@gmail.com>
2017-11-10 15:05:36 -0800

    game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		"rank": "queen",
+		"suit": "hearts",
+		"cardImage": "images/queen-of-hearts.png"
+	},
+	{
+		"rank": "queen",
+		"suit": "diamonds",
+		"cardImage": "images/queen-of-diamonds.png"
+	},
+	{
+		"rank": "king",
+		"suit": "hearts",
+		"cardImage": "images/king-of-hearts.png"
+	},
+	{
+		"rank": "king",
+		"suit": "diamonds",
+		"cardImage": "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 var checkForMatch = function(){
 	if(cardsInPlay.length === 2){
@@ -11,7 +32,7 @@ var checkForMatch = function(){
 	}
 };
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId]);
 	checkForMatch();
 };
