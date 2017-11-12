9348f6a70d48e6c58c8072e1a72241d77b2bb3e7
clairejustinet <claire.backs@gmail.com>
2017-11-12 12:52:46 -0800

    final game

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-
+var wins = 0;
+var losses = 0;
 var cards = [
 	{
 		"rank": "queen",
@@ -26,25 +27,38 @@ var checkForMatch = function(){
 
 	if(cardsInPlay.length === 2){
 		if(cardsInPlay[0] === cardsInPlay[1]){
-			console.log("You found a match!");
+			wins++;
+			alert("You found a match!");
+			document.querySelector('.won').textContent = wins;
 		} else {
-			console.log("Sorry, try again!");
+			losses++;
+			alert("Sorry, try again!");
+			document.querySelector('.lost').textContent = losses;
 		}
 	}
 };
 var flipCard = function(){
 	var cardId = this.getAttribute('data-id');
-	cardsInPlay.push(cards[cardId].rank);
 	this.setAttribute('src', cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
+};
+var resetBoard = function(){
+	var cardImages = document.querySelectorAll('.cardImg');
+	for(var i = 0; i < cardImages.length; i ++ ){
+		cardImages[i].setAttribute('src', "images/back.png");
+	}
+	cardsInPlay = [];
 };
 var createBoard = function(){
 	for(var i = 0; i < cards.length; i++){
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', "images/back.png");
 		cardElement.setAttribute('data-id', i);
+		cardElement.setAttribute('class', 'cardImg');
 		cardElement.addEventListener('click', flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
 	}
+	document.querySelector('.reset').addEventListener('click', resetBoard);
 };
 createBoard();
